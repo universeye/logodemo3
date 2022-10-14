0850c7457da3bcd954e3e299c4fbe40acc706010
terrykuo <terry.501@hotmail.com>
2022-10-14 09:56:29 +0800

    Initial Commit

---
+++ LogoDemo3/LogoDemo3App.swift
@@ -0,0 +1,17 @@
+//
+//  LogoDemo3App.swift
+//  LogoDemo3
+//
+//  Created by Terry Kuo on 2022/10/14.
+//
+
+import SwiftUI
+
+@main
+struct LogoDemo3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
